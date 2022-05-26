62aabb8afd448688cb7b9e8360daec1772f653c0
jude <zpd106@sina.com>
2022-05-26 11:29:17 +0800

    up

--- .umirc.ts
+++ .umirc.ts
@@ -33,6 +33,6 @@ export default defineConfig({
   // more config: https://d.umijs.org/config
   // ssr: isDeploy ? {} : undefined,
   // webpack5: {},
-  // exportStatic: {},
+  exportStatic: {},
   // mfsu: !isDeploy ? {} : undefined,
 });
